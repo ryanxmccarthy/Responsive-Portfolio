aa3d99f40473e526d9fa4854a798c10aa611db7a
Ryan XVX <ryanxmccarthy@gmail.com>
2022-02-03 12:15:55 -0800

    trying to get images to link to github

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -8,18 +8,20 @@ function Cards() {
       <div className='cards__container'>
         <div className='cards__wrapper'>
           <ul className='cards__items'>
-            <Card 
-              src='images/img-1.jpg'
-              text='Explore the hidden waterfall deep inside the Amazon jungle'
-              label='Adventure'
-              path='/services'
-            />
-            <Card 
-              src='images/img-2.jpg'
-              text='Travel through the Islands of Bali in a Private Cruise'
-              label='Luxury'
-              path='/services'
-            />
+          <a href="https://github.com/ryanxmccarthy/react-scrobbler">
+              <Card 
+                src='images/img-1.jpg'
+                text='Responsive Scrobbler'
+                label='React'
+                path='/services'
+              />
+          </a>
+              <Card 
+                src='images/img-2.jpg'
+                text='Travel through the Islands of Bali in a Private Cruise'
+                label='Luxury'
+                path='/services'
+              />
           </ul>
           <ul className='cards__items'>
             <Card 
