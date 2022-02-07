dc9e41281a9051002649ecc19a80286ad046f7d9
Ryan XVX <ryanxmccarthy@gmail.com>
2022-02-07 10:28:38 -0800

    fixed onClick firing on page render

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,12 +9,12 @@ function Hero() {
       {/* <video src='/videos/video-2.mp4' autoPlay loop muted /> */}
       <h1>Ryan McCarthy</h1>
       <p>Web Developer</p>
-      <div className='hero-btns'>
-          <Button
+      <div className='hero-btns'>     
+        <Button
             className='btns'
             buttonStyle='btn--outline'
             buttonSize='btn--large'
-            onClick={window.open('mailto:ryanxmccarthy@gmail.com', '_blank')}
+            onClick={() => {window.open('mailto:ryanxmccarthy@gmail.com', '_blank')}}
           >
             CONTACT ME <i className='fas fa-envelope' />
           </Button>
@@ -22,7 +22,7 @@ function Hero() {
             className='btns'
             buttonStyle='btn--primary'
             buttonSize='btn--large'
-            onClick={window.open('https://github.com/ryanxmccarthy', '_blank')}           
+            // onClick={window.open('https://github.com/ryanxmccarthy', '_blank')}           
           >
             VIEW MY GITHUB <i className='fab fa-github' />
           </Button>
