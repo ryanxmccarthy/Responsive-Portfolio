358e7d846b1bf738f3d850aff8632cf980025c40
Ryan XVX <ryanxmccarthy@gmail.com>
2022-02-28 19:30:40 -0800

    adding sites

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -8,30 +8,30 @@ function Cards() {
       <div className='cards__container'>
         <div className='cards__wrapper'>
           <ul className='cards__items'>
-                <Card 
-                src='images/img-1.jpg'
-                text='Responsive Scrobbler'
-                label='React'
-                link='https://github.com/ryanxmccarthy/react-scrobbler'
+            <Card 
+              src='images/img-2.jpg'
+              text='Banking Website'
+              label='React'
+              link='https://github.com/ryanxmccarthy/todos'
+            />
+            <Card 
+              src='images/img-1.jpg'
+              text='Responsive Scrobbler'
+              label='React'
+              link='https://github.com/ryanxmccarthy/react-scrobbler'
               />         
-              <Card 
+          </ul>
+          <ul className='cards__items'>
+          <Card 
                 src='images/img-2.jpg'
                 text='Todo App'
                 label='React'
                 link='https://github.com/ryanxmccarthy/todos'
               />
-          </ul>
-          <ul className='cards__items'>
             <Card 
               src='images/img-3.jpg'
               text='Explore the hidden waterfall deep inside the Amazon jungle'
               label='Adventure'
-              path='/services'
-            />
-            <Card 
-              src='images/img-4.jpg'
-              text='Travel through the Islands of Bali in a Private Cruise'
-              label='Luxury'
               path='/services'
             />
             {/* <Card 
